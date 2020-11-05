78d1de26eb9380b87ddf85395748637a340ed0c3
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-05 13:39:07 -0600

    feat: created an AdsIndexServlet controller that passes the ad listing to the index jsp.

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -1,2 +1,17 @@
-public class AdsIndexServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AdsIndexServlet" , urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+    }
 }
