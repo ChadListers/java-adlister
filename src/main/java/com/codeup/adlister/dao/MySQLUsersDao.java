94545f46d40e89c7fd044a5cbc696b419bbc1cc6
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-10 13:38:21 -0600

    feat: I added registration form and a registerservlet to allow users to register themselves. The user's info gets added to the database.

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,11 +1,10 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySQLUsersDao implements Users {
     private Connection connection = null;
@@ -29,6 +28,18 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public Long insert(User user) {
-        return null;
+        String sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not create user!");
+        }
     }
 }
