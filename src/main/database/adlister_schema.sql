2b466043954e43e82a08ea1ae3ab5a9d43fc3751
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-06 23:37:12 -0500

    feat: created new directory, new database schema, and category table seeder file

---
+++ src/main/database/adlister_schema.sql
@@ -0,0 +1,44 @@
+# CREATE USER 'chadlister'@'localhost' IDENTIFIED BY 'codeup-1';
+#
+# GRANT ALL ON *.* TO 'chadlister'@'localhost';
+
+DROP DATABASE IF EXISTS chadlister_db;
+
+CREATE DATABASE IF NOT EXISTS chadlister_db;
+
+USE chadlister_db;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS catagories;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    phone_number CHAR(20),
+    is_admin TINYINT DEFAULT '0',
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS category (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED DEFAULT NULL,
+    category_id INT UNSIGNED DEFAULT NULL,
+    title VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    price INT UNSIGNED NOT NULL,
+    image VARCHAR(250),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
