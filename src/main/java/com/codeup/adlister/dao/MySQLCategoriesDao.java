b72331319a833647d673aacf36f6faaae5457ca2
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-07 23:19:00 -0500

    feat: created new CategoriesDao, created and tested all methods, tested good

---
+++ src/main/java/com/codeup/adlister/dao/MySQLCategoriesDao.java
@@ -0,0 +1,112 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Category;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLCategoriesDao implements Categories {
+    private Connection connection = null;
+
+    public MySQLCategoriesDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+
+////// Tested good /////
+    @Override
+    public Long insert(Category category) {
+        try {
+            String insertQuery = "INSERT INTO chadlister_db.category(category) VALUES (?)";
+            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, category.getCategory());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new category.", e);
+        }
+    }
+
+    ////// Tested good /////
+    @Override
+    public void delete(long id) {
+        String query = "DELETE FROM chadlister_db.category WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error deleting category.", e);
+        }
+    }
+
+    ////// Tested good /////
+    @Override
+    public List<Category> all() {
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM chadlister_db.category");
+            ResultSet rs = stmt.executeQuery();
+            return createCategoriesFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all categories.", e);
+        }
+    }
+
+
+    private Category extractCategory(ResultSet rs) throws SQLException {
+        return new Category(
+                rs.getLong("id"),
+                rs.getString("category")
+        );
+    }
+
+    private List<Category> createCategoriesFromResults(ResultSet rs) throws SQLException {
+        List<Category> categories = new ArrayList<>();
+        while (rs.next()) {
+            categories.add(extractCategory(rs));
+        }
+        return categories;
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config();
+        MySQLCategoriesDao categoriesDao = new MySQLCategoriesDao(config);
+
+//        Category category = new Category("cars+trucks");
+//        categoriesDao.insert(category);
+
+//        categoriesDao.delete(1);
+
+        System.out.println(categoriesDao.all());
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
